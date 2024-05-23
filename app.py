d626923d514977e6922dbd44e82f75cf91b7394c
Jason Ramboz <jramboz@gmail.com>
2024-05-22 21:04:14 -0400

    Add beep in tintalle (instead of in py2saber)

--- app.py
+++ app.py
@@ -551,11 +551,15 @@ class Main_Window(QMainWindow, Ui_MainWindow):
     def upload_button_handler(self):
         # Get a list of files to upload. Can be one file or multiple files
         files = QFileDialog.getOpenFileNames(self, filter="RAW Sound File (*.RAW)")[0]
+        files.sort()
+        if self.anima_is_NXT() and not any("BEEP.RAW" in file for file in files) and 'BEEP.RAW' not in self.files_dict.keys():
+            self.log.info('NXT saber detected and no BEEP.RAW provided. Adding default BEEP.RAW.')
+            files.insert(0, os.path.join(basedir, 'OpenCore_OEM', 'BEEP.RAW'))
         self.log.debug(f'List of files to upload: {files}')
         
         if(files):
             # Create and run the upload controller
-            self.uc = Upload_Controller(files, self.sc, parent=self)
+            self.uc = Upload_Controller(files, self.sc, set_effects=True, parent=self)
             try:
                 self.uc.run()
             except Exception as e:
